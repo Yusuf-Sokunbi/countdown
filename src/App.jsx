e0633279580214aa1dff21d0530e1740f0375c17
Yusuf-Sokunbi <adeloaoo87@gmail.com>
2024-09-09 11:55:52 +0100

    Adding TimerChallenge Boxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Player from './components/Player.jsx';
-// import TimerChallenge from './components/TimerChallenge.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges">
-        {/* <TimerChallenge title='Easy' targetTimer={1} />
+        <TimerChallenge title='Easy' targetTimer={1} />
         <TimerChallenge title='Not Easy' targetTimer={5} />
         <TimerChallenge title='Getting tough' targetTimer={10} />
-        <TimerChallenge title= 'Pros only' targetTimer={15} /> */}
+        <TimerChallenge title= 'Pros only' targetTimer={15} />
       </div>
       {/* <TimerChallenge title='yusuf' /> */}
     </>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
+//    let timer;
 
 
 export default function TimerChallenge({title, targetTimer}){
  const [timerExpired, seTimerExpired] = useState(false);
  const [timerstarted, seTimerStarted] = useState(false);
+ const timer = useRef();
 
     function handleStart(){
-        setTimeout(() => {
+       timer.current = setTimeout(() => {
             seTimerExpired(true)
-
-        seTimerStarted(true)
         }, targetTimer * 1000);
-    }
+        seTimerStarted(true);
+    };
+     function handleStop(){
+       clearTimeout(timer.current)
+     }
+
     return ( 
     <section className="challenge">
         <h2>{title}</h2>
-        {/* { timerExpired && <p>You lose</p>} */}
+        { timerExpired && <p>You lose</p>}
         <p className="challenge-time">
             {targetTimer}second {targetTimer > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={handleStart}>
-                {timerstarted ? 'Stop': 'Start'}
+                <button onClick={timerstarted ? handleStop : handleStart}>
+                {timerstarted ? 'Stop': 'Start'} Challenge
                 </button>
             </p>
             <p className={timerstarted ? 'active' : undefined }>
