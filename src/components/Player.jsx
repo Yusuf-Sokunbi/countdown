74e5b49925bda1839e4b42287f8913a92ac884ab
Yusuf-Sokunbi <adeloaoo87@gmail.com>
2024-09-09 09:41:32 +0100

    Complete Input & Button with State and Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,20 @@
-import { useState} from "react";
+import { useState, useRef} from "react";
 
 
 export default function Player() {
-  // const playerName = useRef();
-  const [enterPlayerName, setEnterPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(input){
-    setSubmitted(false)
-    setEnterPlayerName(input.target.value);
-    
-  }
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handleClick(){
-    setSubmitted(true);
-    // input.target.value ="";
+    setEnteredPlayerName(playerName.current.value)
+   playerName.current.value ="";
   }
   return (
     <section id="player">
-      <h2>Welcome { submitted ? enterPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input onChange={handleChange} value={enterPlayerName} type="text" />
+        <input ref={playerName} 
+         type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
